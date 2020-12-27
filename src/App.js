97cef42a16cf61ec21c118441b213fa823cbe898
Jaeyoung Kim <50346571+Jaeyoung-Kim-CND@users.noreply.github.com>
2020-12-26 22:06:02 -0700

    All done, but need to fix left menu top maring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/reports" exact component={Reports} />
+          <Route path="/products" exact component={Products} />
+        </Switch>
+      </Router>
     </div>
   );
 }
